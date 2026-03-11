81fe4119f1815745df4b1a40de7b07582de31abd
sddiana <mosethvatit@gmail.com>
2026-03-11 13:40:52 +0700

    Dedicated the template with data

--- js/main.js
+++ js/main.js
@@ -2,4 +2,31 @@ let eventBus = new Vue()
 
 let app = new Vue({
     el: '#app',
+    data: {
+        cards: [
+            {
+                id: 1,
+                title: 'Тестовая карточка',
+                description: 'Тестовое описание',
+                createdAt: '01.02.2026',
+                deadline: '01.02.2026',
+                column: 1
+            },
+            {
+                id: 2,
+                title: 'Тестовая карточка 2',
+                description: 'Тестовое описание 2',
+                createdAt: '01.02.2026',
+                deadline: '01.02.2026',
+                column: 2
+
+            }
+        ] 
+    },
+
+    methods: {
+        getColumnNumber(columnNumber) {
+            return this.cards.filter(card => card.column === columnNumber)
+        }
+    }
 })
